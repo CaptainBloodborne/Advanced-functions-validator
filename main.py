fb075f5dfc03d0418c9420e5e2a884a8b0b5e586
Tyukina Arina <arina.tyukina@yandex.ru>
2021-03-01 19:42:34 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def validate(low_bound: int, upper_bound: int):
+    pass
+
+
+def set_pixel(red: int, green: int, blue: int) -> str:
+    """A function for creating a pixel."""
+    return "Pixel created!"
